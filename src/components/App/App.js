fa9346cb3228cf3a1b15e6bc5b867efd59403e57
aby varghese alex <abyvalex@gmail.com>
2023-11-15 17:38:01 +0000

    Update 2 files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./App.scss";
+import background from "../../assets/bg_img_v1.png";
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        width: "100%",
+        height: "100vh",
+      }}
+    >
       <Header />
     </div>
   );
