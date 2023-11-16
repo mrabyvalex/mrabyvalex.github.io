346fcb76654a666eaf2cd7dc216229ca71087512
aby varghese alex <abyvalex@gmail.com>
2023-11-16 03:22:14 +0000

    Update 6 files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import "./App.scss";
 import background from "../../assets/bg_img_v1.png";
+import Typewriter from "../TypeWriter/Typewriter";
 
 function App() {
   return (
@@ -11,9 +12,12 @@ function App() {
         backgroundImage: `url(${background})`,
         width: "100%",
         height: "100vh",
+        margin: 0,
+        padding: 0,
       }}
     >
       <Header />
+      <Typewriter />
     </div>
   );
 }
