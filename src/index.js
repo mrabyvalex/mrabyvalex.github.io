75ff49c247f802862bd74be0f8b4f28ee161ed1b
mrabyvalex <abyvalex@gmail.com>
2023-11-16 09:52:27 +0530

    a

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./styles/base-styles.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>?
+  <React.StrictMode>
     <App />
   </React.StrictMode>
 );
